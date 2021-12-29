5cbd02ad92fdfcfb5d51ae44497d25c03c0d38d3
ismaiel <ismaiel.altahan@gmail.com>
2021-12-29 18:01:50 +0100

    - added main style to the to do list

--- src/todoApp.js
+++ src/todoApp.js
@@ -12,23 +12,63 @@ export class TodoApp extends LitElement {
   }
 
   static styles = css`
+    #todo-container {
+      padding: 0px 10px;
+      display: block;
+      width: 50%;
+      margin-left: 25%;
+    }
+    #title {
+      font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+      text-align: center;
+    }
+    .input-item {
+      display: flex;
+      justify-content: space-between;
+      gap: 5px;
+    }
+    .input-item #new-task {
+      font-size: 1em;
+      width: 100%;
+      padding: 5px;
+      flex: 2.5;
+    }
+    .input-item button {
+      cursor: pointer;
+      flex: 0.5;
+    }
+
     ul {
       list-style-type: none;
+      padding-left: 0px;
+      box-shadow: 0px 2px 10px 2px #888888;
+      border-radius: 10px;
     }
+    ul hr {
+      margin: 0px;
+      border: 1px solid #e2e2e2;
+    }
+    ul hr:last-child {
+      display: none;
+    }
+
     .task-item {
-      width: 50%;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: space-between;
+      padding: 15px;
+    }
+    .task-item > div,
+    .task-item button {
       cursor: pointer;
+    }
+    li {
       display: inline;
+      margin: 1em 0;
     }
-    .task-item li {
-      display: inline;
-    }
-    #new-task {
-      font-size: 1.2em;
-    }
-    #task {
+    .task-item #task {
       transform: scale(1.2);
-      margin: 10px;
     }
 
     .completed {
@@ -69,24 +109,28 @@ export class TodoApp extends LitElement {
   }
 
   render() {
-    return html`<div>
-      <h2>Add new task</h2>
-      <input id="new-task" placeholder="What needs to be done ?" type="text" />
-      <button @click=${this._addNewTask}>Add task</button>
+    return html` <h1 id="title">To do list</h1>
+      <div id="todo-container">
+        <div class="input-item">
+          <input id="new-task" placeholder="What needs to be done ?" type="text" />
+          <button @click=${this._addNewTask}>Add task</button>
+        </div>
 
-      <ul>
-        ${this.todoList.map(
-          (task, index) => html` <div>
-            <div class="task-item" @click=${() => this._completeTask(index)}>
-              <input id="task" type="checkbox" />
-              <li class=${task.completed ? "completed" : ""}>${task.task}</li>
-            </div>
-
-            <button @click=${() => this._deleteTask(index)}>delete</button>
-          </div>`
-        )}
-      </ul>
-    </div>`;
+        <ul>
+          ${this.todoList.map(
+            (task, index) => html`
+              <div class="task-item">
+                <div @click=${() => this._completeTask(index)}>
+                  <input id="task" type="checkbox" />
+                  <li class=${task.completed ? "completed" : ""}>${task.task}</li>
+                </div>
+                <button @click=${() => this._deleteTask(index)}>X</button>
+              </div>
+              <hr />
+            `
+          )}
+        </ul>
+      </div>`;
   }
 }
 customElements.define("todo-app", TodoApp);
