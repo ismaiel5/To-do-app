8a551ccf0c9b818b75230009d3c99a025ba5c588
ismaiel <ismaiel.altahan@gmail.com>
2021-12-29 10:55:41 +0100

    - added some mocks data to use as initial data

---
+++ src/mocks/todoList.js
@@ -0,0 +1,7 @@
+export let TASKS = [
+  { task: "Write down my daily tasks", completed: false },
+  { task: "Online shopping", completed: false },
+  { task: "Reading for 30 min", completed: false },
+  { task: "Learn something new", completed: false },
+  { task: "Workout", completed: false },
+];
