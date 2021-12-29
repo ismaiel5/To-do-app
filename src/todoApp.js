40a43e3f9df71e6eb5cea7e58131540214c9870d
ismaiel <ismaiel.altahan@gmail.com>
2021-12-29 11:33:31 +0100

    - added a function to add a new task - added input & button to get the new task

--- src/todoApp.js
+++ src/todoApp.js
@@ -13,9 +13,21 @@ export class TodoApp extends LitElement {
 
   static styles = css``;
 
+  get newTask() {
+    return this.shadowRoot.querySelector("#new-task");
+  }
+  _addNewTask() {
+    this.todoList.push({ task: this.newTask.value, completed: false });
+    this.newTask.value = "";
+    this.requestUpdate();
+  }
+
   render() {
     return html`<div>
       <h2>Add new task</h2>
+      <input id="new-task" placeholder="What needs to be done ?" type="text" />
+      <button @click=${this._addNewTask}>Add task</button>
+
       <ul>
         ${this.todoList.map((task) => html` <li>${task.task}</li>`)}
       </ul>
